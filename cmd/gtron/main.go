6cc72ab852c791bfb7614086178beb669856d7c4
Asuka <yanghang8612@163.com>
2026-07-26 11:16:55 +0800

    perf(pebble): expand dynamic base capacity

--- cmd/gtron/main.go
+++ cmd/gtron/main.go
@@ -199,7 +199,7 @@ var (
 	dbLBaseMaxSizeFlag = &cli.Uint64Flag{
 		Name:  "db.lbase-max-size",
 		Usage: "Pebble dynamic base-level maximum size in MiB",
-		Value: 1024,
+		Value: 2048,
 	}
 	dbL0CompactionFlag = &cli.IntFlag{
 		Name:  "db.l0.compact",
